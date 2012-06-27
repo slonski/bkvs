6402a97063c05d0de416d07b43aa1448f420ce42
Stas Slonski <stas@qlipso.com>
2012-06-28 01:10:59 +0300

    trailer fix

--- app/assets/javascripts/videos.js
+++ app/assets/javascripts/videos.js
@@ -1,10 +1,21 @@
 $(document).ready(function(){
-  $("a#trailer").fancybox({
-  		'hideOnContentClick': false
-  	});
+  $("a#trailer").fancybox();
   $('a img').hover(function(){
     $(this).stop().animate({opacity:1,top:5},200);
   }, function(){
     $(this).stop().animate({opacity:0.8,top:0},200);
   });
+});
+
+
+
+
+$(".fancyYoutube").fancybox({
+'transitionIn' : 'elastic',
+'padding' : 0,
+'autoScale' : false,
+'transitionOut' : 'fade',
+'width' : 680,
+'height' : 495,
+'type' : 'swf'
 });
